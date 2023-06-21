0400b4b9cb37f4fb3af3d9b26d1e5769012c6193
FernandoENF <fernandoengh@gmail.com>
2023-06-21 10:53:01 -0300

    Persisting log in

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,12 @@ import '@/globals.css'
 import type { AppProps } from 'next/app'
 import { Roboto_Flex as Roboto } from 'next/font/google'
 import { Provider } from 'react-redux'
-import store from '@/redux/store'
+import { store, persistor } from '@/redux/store'
 import { DeleteModalProvider } from '@/components/providers/DeleteModalProvider'
 import { EditModalProvider } from '@/components/providers/EditModalProvider'
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 import { Toaster } from 'react-hot-toast'
+import { PersistGate } from 'redux-persist/integration/react'
 
 const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
 
@@ -15,14 +16,16 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
-        <DeleteModalProvider>
-          <EditModalProvider>
-            <main className={`${roboto.variable} font-roboto`}>
-              <Component {...pageProps} />
-              <Toaster />
-            </main>
-          </EditModalProvider>
-        </DeleteModalProvider>
+        <PersistGate loading={null} persistor={persistor}>
+          <DeleteModalProvider>
+            <EditModalProvider>
+              <main className={`${roboto.variable} font-roboto`}>
+                <Component {...pageProps} />
+                <Toaster />
+              </main>
+            </EditModalProvider>
+          </DeleteModalProvider>
+        </PersistGate>
       </Provider>
     </QueryClientProvider>
   )
--- redux/store/index.ts
+++ redux/store/index.ts
@@ -2,12 +2,22 @@ import { legacy_createStore as createStore } from 'redux'
 import rootReducer from '../reducers/rootReducer'
 import { IUser } from '@/redux/reducers/user/types'
 import { IPost } from '@/types/postData'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 export interface IState {
   user: IUser
   post: IPost
 }
 
-const store = createStore(rootReducer)
+const persistConfig = {
+  key: 'root',
+  storage,
+}
 
-export default store
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = createStore(persistedReducer)
+const persistor = persistStore(store)
+
+export { store, persistor }

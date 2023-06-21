2d1e5d6030edcb86a893bbf81c48c3903513cc22
FernandoENF <fernandoengh@gmail.com>
2023-06-21 11:00:42 -0300

    Animations on modal

--- components/modals/DeletePostModal.tsx
+++ components/modals/DeletePostModal.tsx
@@ -1,5 +1,5 @@
-import { FC, useContext, useEffect } from 'react'
-import { Dialog } from '@headlessui/react'
+import { FC, Fragment, useContext } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
 import Button from '@/components/buttons/Button'
 import { DeleteModalContext } from '@/components/providers/DeleteModalProvider'
 import { useSelector } from 'react-redux'
@@ -26,40 +26,59 @@ const DeletePostModal: FC<DeletePostModalProps> = () => {
   }
 
   return (
-    <Dialog
-      open={isOpen ?? false}
-      onClose={() => setIsOpen(false)}
-      className="relative z-50"
-    >
-      {/* The backdrop, rendered as a fixed sibling to the panel container */}
-      <div
-        className="fixed inset-0 bg-light-black-codeleap opacity-80"
-        aria-hidden="true"
-      />
-
-      {/* Full-screen container to center the panel */}
-      <div className="fixed inset-0 flex items-center justify-center p-4">
-        {/* The actual dialog panel  */}
-        <Dialog.Panel className="mx-auto w-[660px] max-w-full space-y-10 rounded-2xl bg-white p-6">
-          <Dialog.Title className="text-[22px] font-bold">
-            Are you sure you want to delete this item?
-          </Dialog.Title>
-          <div className="flex justify-end space-x-8">
-            <Button
-              variant={'secondary'}
-              onClick={() => {
-                setIsOpen(false)
-              }}
-            >
-              Cancel
-            </Button>
-            <Button variant={'danger'} onClick={() => handleDelete()}>
-              Delete
-            </Button>
-          </div>
-        </Dialog.Panel>
-      </div>
-    </Dialog>
+    <Transition appear show={isOpen ?? false} as={Fragment}>
+      <Dialog
+        open={isOpen ?? false}
+        onClose={() => setIsOpen(false)}
+        className="relative z-50"
+      >
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-80"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-80"
+          leaveTo="opacity-0"
+        >
+          <div
+            className="fixed inset-0 bg-light-black-codeleap opacity-80"
+            aria-hidden="true"
+          />
+        </Transition.Child>
+        <div className="fixed inset-0 flex items-center justify-center p-4">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <Dialog.Panel className="mx-auto w-[660px] max-w-full space-y-10 rounded-2xl bg-white p-6">
+              <Dialog.Title className="text-[22px] font-bold">
+                Are you sure you want to delete this item?
+              </Dialog.Title>
+              <div className="flex justify-end space-x-8">
+                <Button
+                  variant={'secondary'}
+                  type={'button'}
+                  onClick={() => {
+                    setIsOpen(false)
+                  }}
+                >
+                  Cancel
+                </Button>
+                <Button variant={'danger'} onClick={() => handleDelete()}>
+                  Delete
+                </Button>
+              </div>
+            </Dialog.Panel>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition>
   )
 }
 export default DeletePostModal
--- components/modals/EditPostModal.tsx
+++ components/modals/EditPostModal.tsx
@@ -1,5 +1,5 @@
-import { FC, useContext, useEffect } from 'react'
-import { Dialog } from '@headlessui/react'
+import { FC, Fragment, useContext, useEffect } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
 import Button from '@/components/buttons/Button'
 import Input from '@/components/inputs/Input'
 import TextArea from '@/components/inputs/TextArea'
@@ -53,50 +53,69 @@ const EditPostModal: FC<EditPostModalProps> = () => {
   }
 
   return (
-    <Dialog
-      open={isOpen ?? false}
-      onClose={() => setIsOpen(false)}
-      className="relative z-50"
-    >
-      <div
-        className="fixed inset-0 bg-light-black-codeleap opacity-80"
-        aria-hidden="true"
-      />
-      <div className="fixed inset-0 flex items-center justify-center p-4">
-        <Dialog.Panel className="mx-auto w-[660px] max-w-full space-y-6 rounded-2xl bg-white p-6">
-          <Dialog.Title className="text-[22px] font-bold">
-            Edit item
-          </Dialog.Title>
-          <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-            <Input
-              label={'Title'}
-              placeholder={'Hello world'}
-              register={register}
-              name={'title'}
-            />
-            <TextArea
-              label={'Content'}
-              placeholder={'Content here'}
-              register={register}
-              name={'content'}
-            />
-            <div className="flex flex-wrap justify-end gap-x-8">
-              <Button
-                variant={'secondary'}
-                onClick={() => {
-                  setIsOpen(false)
-                }}
-              >
-                Cancel
-              </Button>
-              <Button variant={'success'} type="submit">
-                Save
-              </Button>
-            </div>
-          </form>
-        </Dialog.Panel>
-      </div>
-    </Dialog>
+    <Transition appear show={isOpen ?? false} as={Fragment}>
+      <Dialog onClose={() => setIsOpen(false)} className="relative z-50">
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-80"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-80"
+          leaveTo="opacity-0"
+        >
+          <div
+            className="fixed inset-0 bg-light-black-codeleap opacity-80"
+            aria-hidden="true"
+          />
+        </Transition.Child>
+        <div className="fixed inset-0 flex items-center justify-center p-4">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <Dialog.Panel className="mx-auto w-[660px] max-w-full space-y-6 rounded-2xl bg-white p-6">
+              <Dialog.Title className="text-[22px] font-bold">
+                Edit item
+              </Dialog.Title>
+              <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+                <Input
+                  label={'Title'}
+                  placeholder={'Hello world'}
+                  register={register}
+                  name={'title'}
+                />
+                <TextArea
+                  label={'Content'}
+                  placeholder={'Content here'}
+                  register={register}
+                  name={'content'}
+                />
+                <div className="flex flex-wrap justify-end gap-x-8">
+                  <Button
+                    variant={'secondary'}
+                    type={'button'}
+                    onClick={() => {
+                      setIsOpen(false)
+                    }}
+                  >
+                    Cancel
+                  </Button>
+                  <Button variant={'success'} type="submit">
+                    Save
+                  </Button>
+                </div>
+              </form>
+            </Dialog.Panel>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition>
   )
 }
 export default EditPostModal
